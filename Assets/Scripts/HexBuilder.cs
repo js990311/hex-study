0accce40ee2fc833ed8fc91aad4cb79af6ecf182
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-08-06 21:55:05 +0900

    feat: tile map 생성시 offset 좌표계를 기준으로 생성하도록 변경

--- Assets/Scripts/HexBuilder.cs
+++ Assets/Scripts/HexBuilder.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class HexBuilder : MonoBehaviour
 {
+    [SerializeField] private int x_left = -3;
+    [SerializeField] private int x_right = 3;
+    [SerializeField] private int y_top = -3;
+    [SerializeField] private int y_bottom = 3;
+    
     [SerializeField]
     private GameObject hexPrefab;
     
-    private float SIZE = 0.5f; // 중심점부터 육각형의 꼭짓점까지의 거리 
+    private float SIZE = 0.5f; // 중심점부터 육각형의 꼭짓점까지의 거리
 
     private Vector3 Q_BASE_VECTOR = new Vector3(1.5f, -Mathf.Sqrt(3f) / 2, 0f);
     private Vector3 R_BASE_VECTOR = new Vector3(0f, -Mathf.Sqrt(3f), 0f);
@@ -13,23 +18,31 @@ public class HexBuilder : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        CreateHex(0,0);
-        for (int q = -1; q <= 1; q++)
+        for (int x = x_left; x <= x_right; x++)
         {
-            for (int r = -1; r <= 1; r++)
+            for (int y = y_top; y <= y_bottom; y++)
             {
-                if (q == 0 && r == 0)
-                {
-                    continue;
-                }
-                CreateHex(q, r);
+                CreateHex(toAxial(x,y));
             }
         }
     }
 
+    void CreateHex(Vector2Int qr)
+    {
+        CreateHex(qr.x, qr.y);
+    }
+    
     void CreateHex(int q, int r)
     { 
         Vector3 position = SIZE * (q * Q_BASE_VECTOR + r * R_BASE_VECTOR);
         Instantiate(hexPrefab, position, Quaternion.identity, transform);
+    }
+
+    Vector2Int toAxial(int x, int y)
+    {
+        int parity = x & 1;
+        int q = x;
+        int r = y - (x - parity) / 2;
+        return new Vector2Int(q, r);
     }
 }
